bd5160e350554f1cb5848d19e34b4c6d45952441
Alan Reid <alanreidt@gmail.com>
2020-11-04 11:03:26 +0300

    Realize TopBar component view

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="TopBar d-flex py-4 justify-content-center">
+        <Form name="top-bar-form" inline>
+          <Form.Label htmlFor="tag" srOnly>Введите тег</Form.Label>
+          <Form.Control
+            name="tag"
+            id="tag"
+            className="mr-sm-2"
+            placeholder="Введите тег"
+          />
+          <Button className="mr-sm-2" variant="success">Загрузить</Button>
+          <Button className="mr-sm-2" variant="danger">Очистить</Button>
+          <Button>Группировать</Button>
+        </Form>
+      </div>
     </div>
   );
 }
