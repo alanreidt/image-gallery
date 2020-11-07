44ad63601a4d6e6f3656d8b32f0140245659df90
Alan Reid <alanreidt@gmail.com>
2020-11-07 18:42:28 +0300

    Pass ...restProps from HOC to child components (closes #61)

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -5,10 +5,18 @@ export function ImageCard(props: any) {
   const {
     image,
     handleImageCardClick,
+    ...restProps
   } = props;
 
   return (
-    <Card as={'a'} href="#" className="p-1 h-100" data-tag={image.tag} onClick={handleImageCardClick}>
+    <Card
+      as={'a'}
+      href="#"
+      className="p-1 h-100"
+      data-tag={image.tag}
+      onClick={handleImageCardClick}
+      {...restProps}
+    >
       <Card.Img src={image.src} alt={image.alt} style={{ maxHeight: '300px' }} />
     </Card>
   );
