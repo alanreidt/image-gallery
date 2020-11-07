960eb3c5ed287ac68d5e44e6479bbb124bf01c60
Alan Reid <alanreidt@gmail.com>
2020-11-07 10:09:04 +0300

    Prevent the reaction on TagInput empty field (closes #41)

--- src/App.tsx
+++ src/App.tsx
@@ -117,7 +117,7 @@ function App() {
     setImages([]);
   }
 
-  const handleLoadingButtonClick = () => setLoading(true);
+  const handleFormSubmit = () => setLoading(true);
 
   const handleToggleButtonChange = (event: any) => {
     setGrouped(event.currentTarget.checked);
@@ -132,7 +132,7 @@ function App() {
   return (
     <div className="App">
       <div className="TopBar d-flex py-4 justify-content-center">
-        <Form name="top-bar-form" inline>
+        <Form name="top-bar-form" inline onSubmit={handleFormSubmit}>
           <Form.Label htmlFor="tag" srOnly>Введите тег</Form.Label>
           <Form.Control
             name="tag"
@@ -140,12 +140,13 @@ function App() {
             className="mr-2"
             placeholder="Введите тег"
             value={tagInputText}
+            required
             onChange={handleTagInputChange}
           />
           <LoadingButton
+            type="submit"
             className="mr-2"
             isLoading={isLoading}
-            handleClick={handleLoadingButtonClick}
           />
           <Button
             className="mr-2"
